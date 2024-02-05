99f49dda0008f3d56918e20883359a7ba9e00a68
abos-gergo <gergo.abos@students.jedlik.eu>
2024-02-05 11:29:59 +0100

    Currency querying working.

---
+++ CurrencyChange/ViewModel/Commands/QueryCurrenciesCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CurrencyChange.ViewModel.Commands
+{
+    public class QueryCurrenciesCommand : ICommand
+    {
+        public CurrencyChangeVM VM { get; set; }
+        public QueryCurrenciesCommand(CurrencyChangeVM vm) {
+            VM = vm;
+        }
+
+        event EventHandler? ICommand.CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        bool ICommand.CanExecute(object? parameter)
+        {
+            return true;
+
+        }
+
+        void ICommand.Execute(object? parameter)
+        {
+            VM.QueryCurrencies();
+        }
+    }
+}
---
+++ CurrencyChange/ViewModel/CurrencyChangeVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CurrencyChange.Model;
+using CurrencyChange.ViewModel.Commands;
+using CurrencyChange.ViewModel.Helpers;
+
+namespace CurrencyChange.ViewModel
+{
+    public class CurrencyChangeVM : INotifyPropertyChanged
+    {
+        ObservableCollection<Currency> AvailableCurrencies { get; set; }
+        CurrencyConverter currencyConverter = new CurrencyConverter();
+        public QueryCurrenciesCommand? QueryCurrenciesCommand { get; set; }
+
+        public CurrencyChangeVM()
+        {
+            AvailableCurrencies = new ObservableCollection<Currency>();
+            currencyConverter = new CurrencyConverter();
+            List<Currency> currencies = CurrencyChangeHelper.GetCurrencies().GetAwaiter().GetResult();
+            AvailableCurrencies = new ObservableCollection<Currency>(currencies);
+            QueryCurrenciesCommand = new QueryCurrenciesCommand(this);
+        }
+
+        public async void QueryCurrencies()
+        {
+            List<Currency> currencies = await CurrencyChangeHelper.GetCurrencies();
+            AvailableCurrencies = new ObservableCollection<Currency>(currencies);
+        }
+        private void OnPropertyChanged(string v)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(v));
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
